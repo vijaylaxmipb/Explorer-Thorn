57e5957db9dad990b99187a1cf4f4133bd2fbc2c
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-07-12 22:39:23 +0000

    Add boiler point to index.html & install Flask

---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello,</h1> <h2>World</h2>"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
