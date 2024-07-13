3a6d865685c1b9f8770c20969b311842c675b725
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-07-13 16:28:14 +0000

    Add & update render template

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -18,6 +18,11 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
